b59416c2366850074da26b359c39ea5cc38ea622
Stéphane Caron <caron@ynl.t.u-tokyo.ac.jp>
2013-10-08 19:19:31 +0900

    first pass for PEP8 compliance

--- TOPPpy.py
+++ TOPPpy.py
@@ -16,21 +16,21 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-from numpy import *
-from pylab import *
 import StringIO
 import bisect
 
+from pylab import arange, array, double, zeros
+from pylab import gca, plot
 
 
 def ProfileFromLines(lines):
     l = lines[0]
-    [duration,dt] = [double(x) for x in l.split(' ')]
+    [duration, dt] = [double(x) for x in l.split(' ')]
     l = lines[1]
     sarray = array([double(x) for x in l.split(' ')])
     l = lines[2]
     sdarray = array([double(x) for x in l.split(' ')])
-    return [duration,dt,sarray,sdarray]
+    return [duration, dt, sarray, sdarray]
 
 
 def ProfilesFromString(s):
@@ -39,7 +39,7 @@ def ProfilesFromString(s):
     lines = [l.strip(" \n") for l in s.split('\n')]
     n = len(lines) / 3
     for i in range(n):
-        profileslist.append(ProfileFromLines(lines[3*i:3*i+3]))
+        profileslist.append(ProfileFromLines(lines[3 * i:3 * i + 3]))
     return profileslist
 
 
@@ -56,63 +56,64 @@ class Polynomial():
 
     def __init__(self, coeff_vector):
         """
-        Create a polynomial from its list of coefficients (weakest terms first).
+        Create a polynomial from its list of coefficients (weakest terms
+        first).
         """
 
         self.coefficientsvector = coeff_vector
-        self.degree = len(self.coefficientsvector)-1
+        self.degree = len(self.coefficientsvector) - 1
         self.coefficientsvectord = zeros(self.degree)
-        self.coefficientsvectordd = zeros(self.degree-1)
-        for i in range(1,self.degree+1):
-            self.coefficientsvectord[i-1] = i*self.coefficientsvector[i]
-        for i in range(1,self.degree):
-            self.coefficientsvectordd[i-1] = i*self.coefficientsvectord[i]
+        self.coefficientsvectordd = zeros(self.degree - 1)
+        for i in range(1, self.degree + 1):
+            self.coefficientsvectord[i - 1] = i * self.coefficientsvector[i]
+        for i in range(1, self.degree):
+            self.coefficientsvectordd[i - 1] = i * self.coefficientsvectord[i]
 
-    def Eval(self,s):
+    def Eval(self, s):
         res = 0
-        for i in range(self.degree,-1,-1):
-            res = res*s + self.coefficientsvector[i]
+        for i in range(self.degree, -1, -1):
+            res = res * s + self.coefficientsvector[i]
         return res
 
-    def Evald(self,s):
+    def Evald(self, s):
         res = 0
-        for i in range(self.degree-1,-1,-1):
-            res = res*s + self.coefficientsvectord[i]
+        for i in range(self.degree - 1, -1, -1):
+            res = res * s + self.coefficientsvectord[i]
         return res
 
-    def Evaldd(self,s):
+    def Evaldd(self, s):
         res = 0
-        for i in range(self.degree-2,-1,-1):
-            res = res*s + self.coefficientsvectordd[i]
+        for i in range(self.degree - 2, -1, -1):
+            res = res * s + self.coefficientsvectordd[i]
         return res
 
     def GetString(self):
         ss = ""
-        for i in range(0,self.degree+1):
+        for i in range(0, self.degree + 1):
             ss += str(self.coefficientsvector[i])
         return ss
 
 
 class Chunk():
-    def __init__(self,duration,polynomialsvector):
+    def __init__(self, duration, polynomialsvector):
         self.polynomialsvector = polynomialsvector
         self.dimension = len(polynomialsvector)
         self.duration = duration
         self.degree = polynomialsvector[0].degree
 
-    def Eval(self,s):
+    def Eval(self, s):
         q = zeros(self.dimension)
         for i in range(self.dimension):
             q[i] = self.polynomialsvector[i].Eval(s)
         return q
 
-    def Evald(self,s):
+    def Evald(self, s):
         qd = zeros(self.dimension)
         for i in range(self.dimension):
             qd[i] = self.polynomialsvector[i].Evald(s)
         return qd
 
-    def Evaldd(self,s):
+    def Evaldd(self, s):
         qdd = zeros(self.dimension)
         for i in range(self.dimension):
             qdd[i] = self.polynomialsvector[i].Evaldd(s)
@@ -127,7 +128,7 @@ class Chunk():
 
 
 class PiecewisePolynomialTrajectory():
-    def __init__(self,chunkslist):
+    def __init__(self, chunkslist):
         self.chunkslist = chunkslist
         self.dimension = self.chunkslist[0].dimension
         self.degree = self.chunkslist[0].degree
@@ -141,54 +142,54 @@ class PiecewisePolynomialTrajectory():
     def FromString(trajectorystring):
         buff = StringIO.StringIO(trajectorystring)
         chunkslist = []
-        while(buff.pos<buff.len):
+        while buff.pos < buff.len:
             duration = double(buff.readline())
             dimension = int(buff.readline())
-            polynomialsvector = []
+            poly_vector = []
             for i in range(dimension):
-                polynomialsvector.append(Polynomial.FromString(buff.readline()))
-            chunkslist.append(Chunk(duration,polynomialsvector))
+                poly_vector.append(Polynomial.FromString(buff.readline()))
+            chunkslist.append(Chunk(duration, poly_vector))
         return PiecewisePolynomialTrajectory(chunkslist)
 
-    def FindChunkIndex(self,s):
-        if(s==0):
+    def FindChunkIndex(self, s):
+        if s == 0:
             s = 1e-10
-        i = bisect.bisect_left(self.chunkcumulateddurationslist,s)-1
+        i = bisect.bisect_left(self.chunkcumulateddurationslist, s) - 1
         remainder = s - self.chunkcumulateddurationslist[i]
-        return i,remainder
+        return i, remainder
 
-    def Eval(self,s):
-        i,remainder = self.FindChunkIndex(s)
+    def Eval(self, s):
+        i, remainder = self.FindChunkIndex(s)
         return self.chunkslist[i].Eval(remainder)
 
-    def Evald(self,s):
-        i,remainder = self.FindChunkIndex(s)
+    def Evald(self, s):
+        i, remainder = self.FindChunkIndex(s)
         return self.chunkslist[i].Evald(remainder)
 
-    def Evaldd(self,s):
-        i,remainder = self.FindChunkIndex(s)
+    def Evaldd(self, s):
+        i, remainder = self.FindChunkIndex(s)
         return self.chunkslist[i].Evaldd(remainder)
 
-    def Plot(self,dt,f=''):
-        ax=gca()
-        ax.set_color_cycle(['r','g','b'])
-        tvect = arange(0,self.duration+dt,dt)
+    def Plot(self, dt, f=''):
+        ax = gca()
+        ax.set_color_cycle(['r', 'g', 'b'])
+        tvect = arange(0, self.duration + dt, dt)
         qvect = array([self.Eval(t) for t in tvect])
-        plot(tvect,qvect,f)
+        plot(tvect, qvect, f)
 
-    def Plotd(self,dt,f=''):
-        ax=gca()
-        ax.set_color_cycle(['r','g','b'])
-        tvect = arange(0,self.duration+dt,dt)
+    def Plotd(self, dt, f=''):
+        ax = gca()
+        ax.set_color_cycle(['r', 'g', 'b'])
+        tvect = arange(0, self.duration + dt, dt)
         qdvect = array([self.Evald(t) for t in tvect])
-        plot(tvect,qdvect,f)
+        plot(tvect, qdvect, f)
 
-    def Plotdd(self,dt,f=''):
-        ax=gca()
-        ax.set_color_cycle(['r','g','b'])
-        tvect = arange(0,self.duration+dt,dt)
+    def Plotdd(self, dt, f=''):
+        ax = gca()
+        ax.set_color_cycle(['r', 'g', 'b'])
+        tvect = arange(0, self.duration + dt, dt)
         qddvect = array([self.Evaldd(t) for t in tvect])
-        plot(tvect,qddvect,f)
+        plot(tvect, qddvect, f)
 
     def GetString(self):
         return '\n'.join([chunk.GetString() for chunk in self.chunklist])
--- test_kinematic.py
+++ test_kinematic.py
@@ -20,16 +20,16 @@ import TOPPbindings
 import TOPPpy
 import time
 import sys
-from pylab import *
 
+from pylab import arange, array, axis, clf, figure, hold, ion, plot
 
 # Constraints :
-# amax0 , amax1 \n vmax0, vmax1 (vmax = 0 means no velocity constraints)
+# amax0, amax1 \n vmax0, vmax1 (vmax = 0 means no velocity constraints)
 amax0 = 15
 amax1 = 10
 vmax0 = 20
 vmax1 = 10
-constraintstring = "%f %f\n %f %f"%(amax0,amax1,vmax0,vmax1)
+constraintstring = "%f %f\n %f %f" % (amax0, amax1, vmax0, vmax1)
 
 
 # Tunings :
@@ -38,7 +38,7 @@ constraintstring = "%f %f\n %f %f"%(amax0,amax1,vmax0,vmax1)
 # - precision for sdot search around switch points
 # - number of time steps to integrate around dynamic singularities
 # - time step for reparameterization
-tuningsstring = "0.01 0.01 0.01 20 0.01";
+tuningsstring = "0.01 0.01 0.01 20 0.01"
 
 
 # Trajectory :
@@ -47,18 +47,21 @@ tuningsstring = "0.01 0.01 0.01 20 0.01";
 # Coefficients of the polynomial of dof 1 (from lower to higher)
 # Coefficients of the polynomial of dof 2 (from lower to higher) ...
 # Chunk 2 ...
-trajectorystring = "2 \n 2\n 1 1 0 1\n 0 2 0 -1\n 3\n 2\n 11 13 6 0.1666666666666\n -4 -10 -6 0.5";
+trajectorystring = \
+    "2 \n 2\n 1 1 0 1\n 0 2 0 -1\n " + \
+    "3\n 2\n 11 13 6 0.1666666666666\n -4 -10 -6 0.5"
 
 
 # Run TOPP
 start = time.time()
-x = TOPPbindings.TOPPInstance("KinematicLimits",constraintstring,trajectorystring,tuningsstring);
-ret = x.RunPP(1e-4,1e-4)
+x = TOPPbindings.TOPPInstance("KinematicLimits", constraintstring,
+                              trajectorystring, tuningsstring)
+ret = x.RunPP(1e-4, 1e-4)
 
-if(ret==0):
+if ret == 0:
     sys.exit()
 
-print "Computation time: ", (time.time()-start)
+print "Computation time: ", (time.time() - start)
 print "Duration reparameterized trajectory: ", x.resduration
 
 # Run Velocity Interval Propagation
@@ -75,41 +78,41 @@ traj1 = TOPPpy.PiecewisePolynomialTrajectory.FromString(x.restrajectorystring)
 # Verification
 ion()
 dt = 0.1
-tvect = arange(0,traj1.duration+dt,dt)
+tvect = arange(0, traj1.duration + dt, dt)
 qdd = array([traj1.Evaldd(t) for t in tvect])
-print "Max acceleration: ", max(abs(qdd[:,0])) ,"," , max(abs(qdd[:,1]))
-Tmax = max(traj0.duration,traj1.duration)
-Vmax = 1.2*max(vmax0,vmax1)
-Amax = 1.2*max(amax0,amax1)
+print "Max acceleration: ", max(abs(qdd[:, 0])), ",", max(abs(qdd[:, 1]))
+Tmax = max(traj0.duration, traj1.duration)
+Vmax = 1.2 * max(vmax0, vmax1)
+Amax = 1.2 * max(amax0, amax1)
 
 # Plotting
 figure(0)
 clf()
 hold('on')
 traj0.Plot(dt)
-traj1.Plot(dt,'--')
+traj1.Plot(dt, '--')
 
 figure(1)
 clf()
 hold('on')
 traj0.Plotd(dt)
-traj1.Plotd(dt,'--')
-plot([0,Tmax],[vmax0,vmax0],'r-.')
-plot([0,Tmax],[-vmax0,-vmax0],'r-.')
-plot([0,Tmax],[vmax1,vmax1],'g-.')
-plot([0,Tmax],[-vmax1,-vmax1],'g-.')
-axis([0,Tmax,-Vmax,Vmax])
+traj1.Plotd(dt, '--')
+plot([0, Tmax], [vmax0, vmax0], 'r-.')
+plot([0, Tmax], [-vmax0, -vmax0], 'r-.')
+plot([0, Tmax], [vmax1, vmax1], 'g-.')
+plot([0, Tmax], [-vmax1, -vmax1], 'g-.')
+axis([0, Tmax, -Vmax, Vmax])
 
 figure(2)
 clf()
 hold('on')
 traj0.Plotdd(dt)
-traj1.Plotdd(dt,'--')
-plot([0,Tmax],[amax0,amax0],'r-.')
-plot([0,Tmax],[-amax0,-amax0],'r-.')
-plot([0,Tmax],[amax1,amax1],'g-.')
-plot([0,Tmax],[-amax1,-amax1],'g-.')
-axis([0,Tmax,-Amax,Amax])
+traj1.Plotdd(dt, '--')
+plot([0, Tmax], [amax0, amax0], 'r-.')
+plot([0, Tmax], [-amax0, -amax0], 'r-.')
+plot([0, Tmax], [amax1, amax1], 'g-.')
+plot([0, Tmax], [-amax1, -amax1], 'g-.')
+axis([0, Tmax, -Amax, Amax])
 
 x.WriteProfilesList()
 profileslist = TOPPpy.ProfilesFromString(x.resprofilesliststring)
@@ -117,15 +120,12 @@ figure(3)
 clf()
 hold('on')
 mvcbobrow = profileslist.pop(0)
-plot(mvcbobrow[2],mvcbobrow[3],'m--',linewidth=4)
+plot(mvcbobrow[2], mvcbobrow[3], 'm--', linewidth=4)
 mvcdirect = profileslist.pop(0)
-plot(mvcdirect[2],mvcdirect[3],'c--',linewidth=4)
+plot(mvcdirect[2], mvcdirect[3], 'c--', linewidth=4)
 for p in profileslist:
-    plot(p[2],p[3])
+    plot(p[2], p[3])
 
-axis([0,mvcbobrow[0],0,2*max([max(p[3]) for p in profileslist])])
-
-
+axis([0, mvcbobrow[0], 0, 2 * max([max(p[3]) for p in profileslist])])
 
 raw_input()
-
