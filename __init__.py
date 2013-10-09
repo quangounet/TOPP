15a90c6ca92e223666799eb5baee86a9bcf73ada
Stéphane Caron <caron@ynl.t.u-tokyo.ac.jp>
2013-10-09 19:41:57 +0900

    fixed name bug for vect_to_str local function

--- __init__.py
+++ __init__.py
@@ -2,14 +2,14 @@ import pylab
 
 import TOPPbindings
 
-from TOPPpy import PiecewisePolynomialTrajectory
+from TOPPpy import Polynomial, Chunk, PiecewisePolynomialTrajectory
 
 
 class NoTrajectoryFound(Exception):
     pass
 
 
-def __vect_to_str(v):
+def _vect_to_str(v):
     return ' '.join(map(str, v))
 
 
@@ -33,54 +33,55 @@ class Tunings(object):
         self.switchpoint_steps = switchpoint_steps
 
     def __str__(self):
-        kron = [self.mvc_tstep, self.integ_tstep, self.sd_prec]
-        kron.extend([self.switchpoint_steps, self.reparam_tstep])
-        return "%f %f %f %d %f" % kron
+        return "%f %f %f %d %f" % (
+            self.mvc_tstep, self.integ_tstep, self.sd_prec,
+            self.switchpoint_steps, self.reparam_tstep)
 
 
 class TorqueConstraints(object):
     def __init__(self, tau1, tau2=None):
-        self.tau_min = tau1 if tau2 else -tau1
-        self.tau_max = tau2 if tau2 else +tau1
+        self.tau_min = tau1 if tau2 else -1. * tau1
+        self.tau_max = tau2 if tau2 else +1. * tau1
 
     def __str__(self):
-        tau_min_str = __vect_to_str(self.tau_min)
-        tau_max_str = __vect_to_str(self.tau_max)
+        tau_min_str = _vect_to_str(self.tau_min)
+        tau_max_str = _vect_to_str(self.tau_max)
         return "%s\n%s" % (tau_min_str, tau_max_str)
 
 
 class RaveTorqueInstance(object):
     def __init__(self, constraints, openrave_robot, traj, tunings):
+        assert isinstance(constraints, TorqueConstraints)
+        assert isinstance(traj, PiecewisePolynomialTrajectory)
+
         self.constraints = constraints
         self.robot = openrave_robot
         self.tunings = tunings
         self.traj = traj
 
-        self.solver = TOPPbindings.TOPPProblem(
-            "TorqueLimits", self.get_dynamics_str(self.robot), str(self.traj),
+        self.solver = TOPPbindings.TOPPInstance(
+            "TorqueLimits", self.get_dynamics_str(), str(self.traj),
             str(self.tunings))
 
     def get_dynamics_str(self):
-        poly_traj = PiecewisePolynomialTrajectory(str(self.traj))
-        assert abs(poly_traj.duration - self.traj.duration) < 1e-6
-
-        nb_steps = 1 + int((self.duration + 1e-10) / self.tunings.disc_tstep)
-        trange = pylab.arange(nb_steps) * self.tunings.disc_tstep
+        dt = self.tunings.mvc_tstep
+        nb_steps = 1 + int((self.traj.duration + 1e-10) / dt)
+        trange = pylab.arange(nb_steps) * dt
         invdyn_str = ''
 
         for i, t in enumerate(trange):
-            q = poly_traj.Eval(t)
-            qd = poly_traj.Evald(t)
-            qdd = poly_traj.Evaldd(t)
+            q = self.traj.Eval(t)
+            qd = self.traj.Evald(t)
+            qdd = self.traj.Evaldd(t)
             with self.robot:
                 self.robot.SetDOFValues(q)
                 self.robot.SetDOFVelocities(qd)
                 args, kwargs = (qdd, None), {'returncomponents': True}
                 tm, tc, tg = self.robot.ComputeInverseDynamics(*args, **kwargs)
                 to = self.robot.ComputeInverseDynamics(qd) - tc - tg
-                invdyn_str += "\n" + __vect_to_str(to)       # a vector
-                invdyn_str += "\n" + __vect_to_str(tm + tc)  # b vector
-                invdyn_str += "\n" + __vect_to_str(tg)       # c vector
+                invdyn_str += "\n" + _vect_to_str(to)       # a vector
+                invdyn_str += "\n" + _vect_to_str(tm + tc)  # b vector
+                invdyn_str += "\n" + _vect_to_str(tg)       # c vector
 
         return invdyn_str
 
