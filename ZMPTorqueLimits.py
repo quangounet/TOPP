c176801fab3c4657847d57e5f85c0e82254afdaa
Stéphane Caron <caron@ynl.t.u-tokyo.ac.jp>
2013-11-26 20:19:16 +0900

    AVP and Reparameterize shortcut functions in python headers

--- ZMPTorqueLimits.py
+++ ZMPTorqueLimits.py
@@ -5,16 +5,15 @@ from TOPPpy import vect2str
 
 
 class RaveInstance(TOPPpy.RaveInstance):
-    def __init__(self, robot, traj, tunings, activedofs, activelinks, taumin,
+    def __init__(self, robot, traj, activedofs, activelinks, taumin,
                  taumax, zmplimits, vmax, **kwargs):
         super(RaveInstance, self).__init__(robot, traj, taumin, taumax, vmax,
                                            **kwargs)
 
         buffsize = 200000
-        tunstring = "%f %f %f %d" % (self.discrtimestep,
-                                     self.integrationtimestep,
-                                     self.reparamtimestep,
-                                     self.passswitchpointnsteps)
+        tuningsstring = "%f %f %f %d" % (
+            self.discrtimestep, self.integrationtimestep,
+            self.reparamtimestep, self.passswitchpointnsteps)
 
         trajstring = str(traj)
 
@@ -25,14 +24,31 @@ class RaveInstance(TOPPpy.RaveInstance):
         constraintstring += "\n" + vect2str(zmplimits)
         constraintstring += "\n" + vect2str(vmax)
 
-        print "tuningsstring = \"\"\"" + tunstring + "\"\"\"\n"
+        print "tuningsstring = \"\"\"" + tuningsstring + "\"\"\"\n"
         print "constraintstring = \"\"\"" + constraintstring + "\"\"\"\n"
         print "trajectorystring = \"\"\"" + trajstring + "\"\"\"\n"
 
         assert len(constraintstring) < buffsize, \
             "%d is bigger than buffer size" % len(constraintstring)
         assert len(trajstring) < buffsize
-        assert len(tunstring) < buffsize
+        assert len(tuningsstring) < buffsize
 
         self.solver = TOPPbindings.TOPPInstance(
-            "ZMPTorqueLimits", constraintstring, trajstring, tunstring, robot)
+            "ZMPTorqueLimits", constraintstring, trajstring, tuningsstring,
+            robot)
+
+
+def AVP(robot, traj, sdbegmin, sdbegmax, activedofs, activelinks, taumin,
+        taumax, zmplimits, vmax, **kwargs):
+    rave_instance = RaveInstance(
+        robot, traj, activedofs, activelinks, taumin, taumax, zmplimits, vmax,
+        **kwargs)
+    return rave_instance.GetAVP(sdbegmin, sdbegmax)
+
+
+def Reparameterize(robot, traj, sdbegmin, sdbegmax, activedofs, activelinks,
+                   taumin, taumax, zmplimits, vmax, **kwargs):
+    rave_instance = RaveInstance(
+        robot, traj, activedofs, activelinks, taumin, taumax, zmplimits, vmax,
+        **kwargs)
+    return rave_instance.GetTrajectory(sdbegmin, sdbegmax)
