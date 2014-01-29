f01029b183f0aa0cd4327b8cc245be7e86b17194
Stéphane Caron <caron@ynl.t.u-tokyo.ac.jp>
2014-01-29 18:52:19 +0900

    Constraints string for TOPPInstance have been updated. Propagating to ZMPTorqueLimits.

--- ZMPTorqueLimits.py
+++ ZMPTorqueLimits.py
@@ -6,7 +6,7 @@ from TOPPpy import vect2str
 
 class RaveInstance(TOPPpy.RaveInstance):
     def __init__(self, robot, traj, activedofs, activelinks, taumin,
-                 taumax, zmplimits, vmax, **kwargs):
+                 taumax, zmplimits, vmax, qdefault, **kwargs):
         super(RaveInstance, self).__init__(robot, traj, taumin, taumax, vmax,
                                            **kwargs)
 
@@ -15,34 +15,33 @@ class RaveInstance(TOPPpy.RaveInstance):
             self.discrtimestep, self.integrationtimestep,
             self.reparamtimestep, self.passswitchpointnsteps)
 
-        trajstring = str(traj)
-
         constraintstring = vect2str(activedofs)
         constraintstring += "\n" + vect2str(activelinks)
         constraintstring += "\n" + vect2str(taumin)
         constraintstring += "\n" + vect2str(taumax)
         constraintstring += "\n" + vect2str(zmplimits)
         constraintstring += "\n" + vect2str(vmax)
+        constraintstring += "\n" + vect2str(qdefault)
 
         print "tuningsstring = \"\"\"" + tuningsstring + "\"\"\"\n"
         print "constraintstring = \"\"\"" + constraintstring + "\"\"\"\n"
-        print "trajectorystring = \"\"\"" + trajstring + "\"\"\"\n"
+        print "trajectorystring = \"\"\"" + str(traj) + "\"\"\"\n"
 
         assert len(constraintstring) < buffsize, \
             "%d is bigger than buffer size" % len(constraintstring)
-        assert len(trajstring) < buffsize
+        assert len(str(traj)) < buffsize
         assert len(tuningsstring) < buffsize
 
         self.solver = TOPPbindings.TOPPInstance(
-            "ZMPTorqueLimits", constraintstring, trajstring, tuningsstring,
+            "ZMPTorqueLimits", constraintstring, str(traj), tuningsstring,
             robot)
 
 
 def AVP(robot, traj, sdbegmin, sdbegmax, activedofs, activelinks, taumin,
-        taumax, zmplimits, vmax, **kwargs):
+        taumax, zmplimits, vmax, qdefault, **kwargs):
     rave_instance = RaveInstance(
         robot, traj, activedofs, activelinks, taumin, taumax, zmplimits, vmax,
-        **kwargs)
+        qdefault, **kwargs)
     return rave_instance.GetAVP(sdbegmin, sdbegmax)
 
 
