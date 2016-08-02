834f9ff74881511b76fad8e86e85fadde7f8d707
Andreas Orthey <andreas.orthey@gmx.de>
2016-08-01 20:03:41 -0400

    np adjust

--- src/python/Utilities.py
+++ src/python/Utilities.py
@@ -6,7 +6,7 @@ def InterpolateViapoints(path):
     nviapoints = len(path[0,:])
     tv = linspace(0,1,nviapoints)
     for i in range(nviapoints-1):
-        tv[i+1] = tv[i]+np.linalg.norm(path[:,i]-path[:,i+1])
+        tv[i+1] = tv[i]+linalg.norm(path[:,i]-path[:,i+1])
     tcklist = []
     for idof in range(0,path.shape[0]):
         tcklist.append(interpolate.splrep(tv,path[idof,:],s=0))
